9911e0058793a5a87577c41e6eb33ea3d7392bca
kartik macbook <gaurdeveloper1916@gmail.com>
2025-01-11 09:45:22 +0530

    add fd

---
+++ src/app/component/weddingpage/v2.1/Sectionone.jsx
@@ -0,0 +1,12 @@
+const Sectionone = () => {
+    return (
+        <>
+            <div>
+                <video autoplay muted loop id="myVideo">
+                    <source src="https://destinationbrides.com/wp-content/uploads/2022/12/Weddings-Page-Video-November-2022.mp4" type="video/mp4" />
+                </video>
+            </div>
+        </>
+    )
+}
+export default Sectionone;
--- src/app/wedding/page.jsx
+++ src/app/wedding/page.jsx
@@ -1,142 +1,155 @@
-import React from "react";
-import Header from "../component/common/header/Header";
-import Footer from "../component/common/footer/Footer";
-import "./index.css";
-import WeddinfSlider from "../component/weddingpage/WeddinfSlider";
-import ServicesSlider from "../component/weddingpage/ServicesSlider";
-import GallerySlider from "../component/weddingpage/GallerySlider";
-import MultiSlider from "../component/weddingpage/MultiSlider";
-import Moodboard from "../component/weddingpage/Moodboard";
-import WeddingSlider from "../component/weddingpage/OnScrollDiv/WeddingSlider";
-import OnScrollDiv from "../component/weddingpage/OnScrollDiv/OnScrollDiv";
-import FridayNight from "../component/homepage/friday/FridayNight";
-const page = (props) => {
-  return (
-    <div className="bg-black ">
-      <Header />
-      <div
-        className=""
-        style={{
-          backgroundImage: "url('/images/wedding1.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          height: "100vh",
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
-          flexDirection: "column",
-          textAlign: "center",
-        }}
-      >
-        <div className="mt-5 pt-5 d-flex flex-lg-row flex-wrap gap-4 justify-content-center  align-items-start">
-          <div class="dropdown mx-2">
-            <div
-              class="dropbtn dropdown-toggle border-bottom"
-              data-bs-toggle="dropdown-menu"
-              aria-expanded="false"
-            >
-              Desired Date
-            </div>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="dropdown">
-            <div
-              class="dropbtn dropdown-toggle border-bottom"
-              data-bs-toggle="dropdown-menu"
-              aria-expanded="false"
-            >
-              Destination
-            </div>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div class="dropdown">
-            <div
-              class="dropbtn dropdown-toggle border-bottom"
-              data-bs-toggle="dropdown-menu"
-              aria-expanded="false"
-            >
-              Guests
-            </div>
-            <ul class="dropdown-menu">
-              <li>
-                <a class="dropdown-item" href="#">
-                  Action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Another action
-                </a>
-              </li>
-              <li>
-                <a class="dropdown-item" href="#">
-                  Something else here
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="dropbtn ">Get started</div>
-        </div>
+// import React from "react";
+// import Header from "../component/common/header/Header";
+// import Footer from "../component/common/footer/Footer";
+// import "./index.css";
+// import WeddinfSlider from "../component/weddingpage/WeddinfSlider";
+// import ServicesSlider from "../component/weddingpage/ServicesSlider";
+// import GallerySlider from "../component/weddingpage/GallerySlider";
+// import MultiSlider from "../component/weddingpage/MultiSlider";
+// import Moodboard from "../component/weddingpage/Moodboard";
+// import WeddingSlider from "../component/weddingpage/OnScrollDiv/WeddingSlider";
+// import OnScrollDiv from "../component/weddingpage/OnScrollDiv/OnScrollDiv";
+// import FridayNight from "../component/homepage/friday/FridayNight";
+// const page = (props) => {
+//   return (
+//     <div className="bg-black ">
+//       <Header />
+//       <div
+//         className=""
+//         style={{
+//           backgroundImage: "url('/images/wedding1.png')",
+//           backgroundSize: "cover",
+//           backgroundPosition: "center",
+//           height: "100vh",
+//           width: "100%",
+//           display: "flex",
+//           justifyContent: "space-between",
+//           alignItems: "center",
+//           flexDirection: "column",
+//           textAlign: "center",
+//         }}
+//       >
+//         <div className="mt-5 pt-5 d-flex flex-lg-row flex-wrap gap-4 justify-content-center  align-items-start">
+//           <div class="dropdown mx-2">
+//             <div
+//               class="dropbtn dropdown-toggle border-bottom"
+//               data-bs-toggle="dropdown-menu"
+//               aria-expanded="false"
+//             >
+//               Desired Date
+//             </div>
+//             <ul class="dropdown-menu">
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Another action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Something else here
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//           <div class="dropdown">
+//             <div
+//               class="dropbtn dropdown-toggle border-bottom"
+//               data-bs-toggle="dropdown-menu"
+//               aria-expanded="false"
+//             >
+//               Destination
+//             </div>
+//             <ul class="dropdown-menu">
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Another action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Something else here
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//           <div class="dropdown">
+//             <div
+//               class="dropbtn dropdown-toggle border-bottom"
+//               data-bs-toggle="dropdown-menu"
+//               aria-expanded="false"
+//             >
+//               Guests
+//             </div>
+//             <ul class="dropdown-menu">
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Another action
+//                 </a>
+//               </li>
+//               <li>
+//                 <a class="dropdown-item" href="#">
+//                   Something else here
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//           <div className="dropbtn ">Get started</div>
+//         </div>
 
-        <h1 className="text-white vict-text">VICTORIA HITT</h1>
-      </div>
+import Sectionone from "../component/weddingpage/v2.1/Sectionone";
 
-      <WeddinfSlider />
+//         <h1 className="text-white vict-text">VICTORIA HITT</h1>
+//       </div>
 
-      <OnScrollDiv/>
+//       <WeddinfSlider />
 
-      <Moodboard />
+//       <OnScrollDiv/>
 
-      <div className=" d-flex justify-content-center align-items-center flex-column text-white margins ">
-        <h1>Inspirations</h1>
+//       <Moodboard />
 
-        <h1 className="discover">Discover Our services</h1>
-        <ServicesSlider />
-      </div>
+//       <div className=" d-flex justify-content-center align-items-center flex-column text-white margins ">
+//         <h1>Inspirations</h1>
+
+//         <h1 className="discover">Discover Our services</h1>
+//         <ServicesSlider />
+//       </div>
 
      
-        <GallerySlider />
+//         <GallerySlider />
      
-        <FridayNight/>
-        <MultiSlider />
+//         <FridayNight/>
+//         <MultiSlider />
       
 
      
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
+// export default page;
+
+
+const page =()=>{
+  return(
+    <>
+        <p>wedding page </p>
+        <Sectionone/>
+    </>
+  )
+}
 export default page;
