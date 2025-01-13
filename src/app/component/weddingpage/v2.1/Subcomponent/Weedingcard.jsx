7e91c557ea1851e77561d88d76eaf1bd9f5bbc46
kartik macbook <gaurdeveloper1916@gmail.com>
2025-01-13 23:25:20 +0530

    add chnages

--- src/app/component/weddingpage/v2.1/Subcomponent/Weedingcard.jsx
+++ src/app/component/weddingpage/v2.1/Subcomponent/Weedingcard.jsx
@@ -7,12 +7,26 @@ const WeddingCard = ({ margin }) => {
       className={`rounded-4 text-white mx-4 ${margin ? "margin-custom" : ""
         }`}
     >
-      <img
-        src="https://media.istockphoto.com/id/1933376240/photo/portrait-of-a-bride-holding-her-bouquet-on-beach-wedding.jpg?s=612x612&w=0&k=20&c=Bu-SxK2KZtKFf5zeawkobT5KTj32HcvDwRPH7-1WxkY="
-        className="  rounded-image px-4"
-        alt="..."
-      />
-      <div className="d-flex gap-2">
+      <div
+        alt="Bride holding bouquet"
+        style={{
+          width: "230px",
+          height: "230px",
+          borderStyle: "dotted",
+          borderWidth: "2px",
+          borderColor: "black",
+        }}
+        className="rounded-circle p-2 d-flex justify-content-center align-items-center"
+      >
+        <img
+          src="https://media.istockphoto.com/id/1933376240/photo/portrait-of-a-bride-holding-her-bouquet-on-beach-wedding.jpg?s=612x612&w=0&k=20&c=Bu-SxK2KZtKFf5zeawkobT5KTj32HcvDwRPH7-1WxkY="
+          className="rounded-circle"
+          alt="Bride holding bouquet"
+          style={{ width: "200px", height: "200px" }}
+        />
+      </div>
+
+      <div className="d-flex gap-2 mt-4">
         <p className="text-black">01 </p>
         <div className="d-flex flex-column">
           <p className="text-black m-0">Connect</p>
--- src/app/wedding/page.jsx
+++ src/app/wedding/page.jsx
@@ -154,7 +154,7 @@ const page =()=>{
     <>
     <Header/>
         <Sectionone/>
-        <Sectiontwo/>
+        {/* <Sectiontwo/> */}
         <Sectionthree/>
 
     </>
