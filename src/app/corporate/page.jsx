e45cccf374089b1d50ba188057480946c3674b43
kartik <gaurdeveloper1916@gmail.com>
2025-01-25 15:40:21 +0530

    add chnages

--- src/app/corporate/page.jsx
+++ src/app/corporate/page.jsx
@@ -1,8 +1,5 @@
 "use client";
-import SectionOne from "@/app/component/corporate/v2.1/components/SectionOne";
-import SectionTwo from "@/app/component/corporate/v2.1/components/SectionTwo";
-import SectionFour from "@/app/component/corporate/v2.1/components/SectionThree";
-import SectionFive from "@/app/component/corporate/v2.1/components/SectionFour";
+
 import SectionSix from "@/app/component/corporate/v2.1/components/SectionFive";
 import SectionSeven from "@/app/component/corporate/v2.1/components/SectionSix";
 import SectionThree from "@/app/component/corporate/v2.1/components/SectionSeven";
@@ -12,14 +9,10 @@ import Header from "../component/common/header/Header";
 const CorporatePage = () => {
   return (
     <section className="min-vh-100">
-      {/* <Header/> */}
-      <SectionOne />
-      <SectionTwo />
-      <SectionFour />
-      <SectionFive />
+      <Header/>
+     
       <SectionSix />
       <SectionSeven />
-            {/* <SectionFive /> */}
 
       <SectionThree />
     </section>
