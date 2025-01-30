8196128f87a748cca5d659769bb6fb23e773f283
kartik <gaurdeveloper1916@gmail.com>
2025-01-30 09:48:17 +0530

    add chnages

---
+++ src/app/component/mnm/Repetativemandm.jsx
@@ -0,0 +1,77 @@
+const Repetativemandm = () => {
+    return (
+        <>
+            <div className="d-flex justify-content-center my-5">
+                <div
+                    style={{
+                        backgroundImage: "url('https://plus.unsplash.com/premium_photo-1671580668731-b94e681ccd6c?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                    className="col-lg-11 text-center px-5 py-3 rounded"
+                >
+                    <h1 className="colo-bg fw-bold largefont text-start text-white fs-4">
+                        MEDIA AND MARKETING WITH DEMIGOD HOUSE
+                    </h1>
+                    <p className="text-white text-start">
+                        Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events!  Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events!  Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events!  Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events!  Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events!  Brand Developement & Marketing, Print-media & Electronic-media,
+                        Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B /
+                        B2C Events! (repeat content)
+                    </p>
+                    <div className="d-flex justify-content-center align-items-center"></div>
+                </div>
+            </div>
+
+            <div className="d-flex flex-column">
+                <div
+                    className="d-flex gap-5 px-5 py-5 text-white  justify-content-center"
+                    style={{
+                        backgroundImage: "url('https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                    }}
+                >
+                    <div className="col-lg-8">
+                        <h1 className="fw-bold gradient-text">Demigod Events</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti aut odit, iste tempora facere eveniet eaque reprehenderit nesciunt voluptatem harum pariatur...
+
+
+                        </p>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="d-flex gap-5 mb-5">
+                            <img className="img-fluid w-25 h-25 rounded" src="./123.jpg" />
+                            <img className="img-fluid w-25 h-25 rounded" src="./1234.jpg" />
+                        </div>
+                        <div className="d-flex gap-5">
+                            <img className="img-fluid w-25 h-25 rounded" src="https://cdn-icons-png.flaticon.com/128/6980/6980679.png" />
+                            <img className="img-fluid w-25 h-25 rounded" src="./123.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </>
+    )
+}
+export default Repetativemandm;
