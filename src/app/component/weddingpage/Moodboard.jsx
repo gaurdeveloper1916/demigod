da1265b528f0c109462352ad3dac2065c76a9922
kartik <gaurdeveloper1916@gmail.com>
2025-02-14 12:48:07 +0530

    add chnages

--- src/app/component/weddingpage/Moodboard.jsx
+++ src/app/component/weddingpage/Moodboard.jsx
@@ -6,87 +6,88 @@ import { MdArrowOutward } from "react-icons/md";
 function Moodboard() {
   const [showModal, setShowModal] = useState(false);
   const [selectedImage, setSelectedImage] = useState("");
-
-  const handleImageClick = (imageSrc) => {
+  const [des,setDes]=useState('')
+  const handleImageClick = (imageSrc,heading) => {
     setSelectedImage(imageSrc);
     setShowModal(true);
+    setDes(heading)
   };
 
   const handleClose = () => setShowModal(false);
 
   return (
     <div className="container-fluid py-5 bg-black d-flex justify-content-center align-items-center flex-column text-white margins">
-  
-      <div className="col-lg-8 d-flex flex-column align-items-center justify-content-center">
+
+      <div className="col-lg-10 d-flex flex-column align-items-center justify-content-center">
         <h1 className="moodboard">traditional </h1>
-  <div className="row d-flex flex-wrap align-items-center justify-content-center">
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/telgu.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("/image/wedding-page/mood-1/telgu.webp")}
-        style={{ cursor: "pointer" , width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/punjabi.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("Image/wedding-page/mood-1/punjabi.webp")}
-        style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/marwari.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("Image/wedding-page/mood-1/marwari.webp")}
-        style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <div>
-        <img
-          alt="image"
-          src="/images/chris.webp"
-          className="img-fluid"
-          onClick={() => handleImageClick("Image/wedding-page/mood-1/chris.webp")}
-          style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-        />
-      <h1 className="text-white">Christian Wedding</h1>
-      </div>
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/bengali.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("Image/wedding-page/mood-1/bengali.webp")}
-        style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/marathi.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("Image/wedding-page/mood-1/marathi.webp")}
-        style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-    <div className="col-5 col-sm-6 col-md-4 col-lg-2">
-      <img
-        alt="image"
-        src="/images/arabic.webp"
-        className="img-fluid"
-        onClick={() => handleImageClick("Image/wedding-page/mood-1/arabic.webp")}
-        style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
-      />
-    </div>
-</div>
+        <div className=" d-flex  gap-3">
+          <div className="">
+            <img
+              alt="image"
+              src="/images/telgu.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("/image/wedding-page/mood-1/telgu.webp","telgu content")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+          <div className="">
+            <img
+              alt="image"
+              src="/images/punjabi.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("Image/wedding-page/mood-1/punjabi.webp")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+          <div className="">
+            <img
+              alt="image"
+              src="/images/marwari.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("Image/wedding-page/mood-1/marwari.webp")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+          <div className="">
+            <div>
+              <img
+                alt="image"
+                src="/images/chris.webp"
+                className="img-fluid"
+                onClick={() => handleImageClick("Image/wedding-page/mood-1/chris.webp")}
+                style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+              />
+              <h1 className="text-white">Christian Wedding</h1>
+            </div>
+          </div>
+          <div className="">
+            <img
+              alt="image"
+              src="/images/bengali.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("Image/wedding-page/mood-1/bengali.webp")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+          <div className="">
+            <img
+              alt="image"
+              src="/images/marathi.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("Image/wedding-page/mood-1/marathi.webp")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+          <div className="">
+            <img
+              alt="image"
+              src="/Image/arabic.webp"
+              className="img-fluid"
+              onClick={() => handleImageClick("Image/wedding-page/mood-1/arabic.webp")}
+              style={{ cursor: "pointer", width: "300px", height: "250px", objectFit: "cover" }}
+            />
+          </div>
+        </div>
 
       </div>
 
@@ -109,7 +110,7 @@ function Moodboard() {
                 <div className="d-flex flex-column">
                   <h1 className="text-white">Bengali Wedding</h1>
                   <p className="text-white">
-                    Bengali wedding 
+                    {des? des:"please pass des"}
                   </p>
                 </div>
               </div>
@@ -118,15 +119,15 @@ function Moodboard() {
             {/* Book Now and Skip buttons positioned at the bottom */}
             <div style={{ position: 'absolute', bottom: 20, left: 20 }}>
               <div className="d-flex">
-              <p className="text-white">Book Now</p>
-              <MdArrowOutward size={25} color="white" className="text-end" />
+                <p className="text-white">Book Now</p>
+                <MdArrowOutward size={25} color="white" className="text-end" />
               </div>
 
             </div>
             <div style={{ position: 'absolute', bottom: 20, right: 20 }}>
-            <div onClick={handleClose} className="d-flex cursor-pointer">
-              <p className="text-white">Slip</p>
-              <MdArrowOutward size={25} color="white" className="text-end" />
+              <div onClick={handleClose} className="d-flex cursor-pointer">
+                <p className="text-white">Slip</p>
+                <MdArrowOutward size={25} color="white" className="text-end" />
               </div>
 
             </div>
