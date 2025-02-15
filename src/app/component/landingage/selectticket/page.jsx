f0a58758be3488ce05b6c4604ae695ef0c4ae5ac
kartik <gaurdeveloper1916@gmail.com>
2025-02-15 12:00:14 +0530

    add chnages

--- src/app/component/landingage/selectticket/page.jsx
+++ src/app/component/landingage/selectticket/page.jsx
@@ -4,6 +4,7 @@ import { FiMinus } from "react-icons/fi";
 import { GoPlus } from "react-icons/go";
 import { IoArrowBackSharp } from "react-icons/io5";
 import { Auth } from "../../../../context/AppContext";
+import './index.css'
 const Selectticket = () => {
     const {landingHeading,landingDes,ticketdes } =useContext(Auth)
     const [ticketCount, setTicketCount] = useState(1);
@@ -11,18 +12,19 @@ const Selectticket = () => {
     return (
         <>
             <div className="d-flex justify-content-center align-items-center bg-black">
-                <div className="col-lg-8">
-                    <div className="d-flex flex-lg-row gap-4 flex-column justify-content-center">
-                        <div className="col-lg-6">
+                <div className="col-lg-8 col-12">
+                    <div className="d-flex flex-lg-row gap-4 p-3 flex-column justify-content-center">
+                        <div className="col-lg-6 col-12">
                             <div className="d-flex gap-2">
-                                <div className="d-flex align-items-center">
-                                    <IoArrowBackSharp color="white" className="mt-1" />
-                                    <button onClick={() => window.history.back()} className="btn btn-link text-white p-0">Go Back</button>
+                                <div className="d-flex align-items-center  gap-2 py-2">
+                                    <IoArrowBackSharp color="white" className=""  size={24}/>
+                                    <button onClick={() => window.history.back()} className=" border rounded px-2 border-0 text-black fs-6">Go back</button>
+                                    
                                 </div>
                             </div>
 
-                            <div style={{ lineHeight: '40px' }}>
-                                <p style={{ fontSize: '30px', fontWeight: '700', whiteSpace: 'nowrap' }} className="m-.25 text-white">{landingHeading}</p>
+                            <div className="leandingtext-container">
+                                <p className="m-.25 text-white leandingtext">{landingHeading}</p>
                             </div>
                             <p className="text-white">ALREADY REGISTER ? <span style={{ color: 'orange' }}>LOG IN</span></p>
                             <div className="mt-5 d-flex flex-column">
@@ -35,7 +37,7 @@ const Selectticket = () => {
                         </div>
 
                         
-                        <div className="col-lg-6">
+                        <div className="col-lg-6 col-12">
                             <h4 style={{ color: 'orange' }} className="m-0">Select your ticket type</h4>
 
                             <p className="text-white">Are you attending virtually or in-person?</p>
