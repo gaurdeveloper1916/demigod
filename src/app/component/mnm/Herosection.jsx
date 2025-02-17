cead7dd086cf743a4d99a2c4c00c60c3d01fed93
kartik <gaurdeveloper1916@gmail.com>
2025-02-17 23:35:53 +0530

    add chnages

--- src/app/component/mnm/Herosection.jsx
+++ src/app/component/mnm/Herosection.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { MdArrowOutward } from "react-icons/md";
-import Link from "next/link";
+
 
 function Herosection() {
   return (
@@ -22,30 +21,9 @@ function Herosection() {
         <h3><p className="text-white">
         Brand Developement & Marketing, Print-media & Electronic-media, Advertising, Brand Seed Funding, Brand Private-Equity Investments & B2B / B2C Events!
         </p></h3>
-        <div className="d-flex justify-content-center align-items-center">
-          {/* <button
-            type="button"
-            className="mx-5 px-4 py-2 border-0 verySmallFont rounded-pill cursor button-bg text-white fw-bold"
-          >
-            Know More
-          </button> */}
-        </div>
+       
       </div>
-      {/* <div
-        className="d-flex w-100 text-white justify-content-between px-5 position-absolute"
-        style={{ bottom: '20px' }}
-      >
-        <div className="d-flex align-items-center">
-          <Link href='/contact-us' className="mediumfont text-deco text-white">
-            Contact
-          </Link>
-          <MdArrowOutward size={21} />
-        </div>
-        <div className="d-flex align-items-center">
-          <p className="mediumfont m-0">Scroll</p>
-          <MdArrowOutward size={21} />
-        </div>
-      </div> */}
+    
     </div>
   );
 }
