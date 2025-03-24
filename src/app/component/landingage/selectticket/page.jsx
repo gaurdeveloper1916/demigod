a4076c5befc99acd0093b2481a21e39ee03436f6
kartik <gaurdeveloper1916@gmail.com>
2025-03-24 14:06:22 +0530

    add chnages

--- src/app/component/landingage/selectticket/page.jsx
+++ src/app/component/landingage/selectticket/page.jsx
@@ -6,9 +6,9 @@ import { IoArrowBackSharp } from "react-icons/io5";
 import { Auth } from "../../../../context/AppContext";
 import './index.css'
 const Selectticket = () => {
-    const {landingHeading,landingDes,ticketdes } =useContext(Auth)
+    const { landingHeading, landingDes, ticketdes } = useContext(Auth)
     const [ticketCount, setTicketCount] = useState(1);
-console.log(landingHeading)
+    console.log(landingHeading)
     return (
         <>
             <div className="d-flex justify-content-center align-items-center bg-black">
@@ -17,26 +17,26 @@ console.log(landingHeading)
                         <div className="col-lg-6 col-12">
                             <div className="d-flex gap-2">
                                 <div className="d-flex align-items-center  gap-2 py-2">
-                                    <IoArrowBackSharp color="white" className=""  size={24}/>
+                                    <IoArrowBackSharp color="white" className="" size={24} />
                                     <button onClick={() => window.history.back()} className=" border rounded px-2 border-0 text-black fs-6">Go back</button>
-                                    
+
                                 </div>
                             </div>
 
                             <div className="leandingtext-container">
-                                <p className="m-.25 text-white leandingtext">{landingHeading}</p>
+                                <p className=" text-white leandingtext">{landingHeading}</p>
                             </div>
                             <p className="text-white">ALREADY REGISTER ? <span style={{ color: 'orange' }}>LOG IN</span></p>
-                            <div className="mt-5 d-flex flex-column">
-                                <p className="text-white">TICKET TYPE:VIRTUAL</p>
-                                <p className="text-white">EMAIL: info@demigodhouse.com</p>
-                                <p className="text-white">TOTAL:FREE</p>
-                                <p className="text-white mt-3">{ticketdes ? ticketdes : 'More Details'} 
+                            <div className="d-flex flex-column">
+                                <p className="text-white m-0">Ticket type : Virtual</p>
+                                <p className="text-white m-0">Email : info@demigodhouse.com</p>
+                                <p className="text-white m-0">Total : ${500 * ticketCount}</p>
+                                <p className="text-white mt-4">{ticketdes ? ticketdes : 'More Details'}
                                 </p>
                             </div>
                         </div>
 
-                        
+
                         <div className="col-lg-6 col-12">
                             <h4 style={{ color: 'orange' }} className="m-0">Select your ticket type</h4>
 
@@ -46,23 +46,23 @@ console.log(landingHeading)
                                     <p className="m-0 text-white">In-Person</p>
                                     <p className="text-secondary"> New York, NY</p>
                                 </div>
-                                <button onClick={() => alert('Button clicked!')} className="mb-3 border rounded px-2 border-0 text-white fs-6">${500*ticketCount}</button>
+                                <button onClick={() => alert('Button clicked!')} className="mb-3 border rounded px-2 border-0 text-white fs-6">${500 * ticketCount}</button>
                             </div>
 
                             <div className="d-flex gap-2 align-items-center my-4">
-                                <div onClick={() => setTicketCount(ticketCount > 1 ? ticketCount - 1 : 1)} 
-                                style={{
-                                    backgroundColor: 'orange',
-                                    width: '50px',
-                                    height: '50px',
-                                    borderRadius: '50%',
-                                    display: 'flex',
-                                    alignItems: 'center',
-                                    justifyContent: 'center',
-                                    cursor: 'pointer'
-                                }} 
-                                className="px-3 rounded-circle py-2"> <FiMinus color="white" /></div>
-                                <p className="mt-3 text-white">{ticketCount}</p>
+                                <div onClick={() => setTicketCount(ticketCount > 1 ? ticketCount - 1 : 1)}
+                                    style={{
+                                        backgroundColor: 'orange',
+                                        width: '50px',
+                                        height: '50px',
+                                        borderRadius: '50%',
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        cursor: 'pointer'
+                                    }}
+                                    className="px-3 rounded-circle py-2"> <FiMinus color="white" /></div>
+                                <p className=" text-white mt-3">{ticketCount}</p>
                                 <div onClick={() => setTicketCount(ticketCount + 1)} style={{
                                     backgroundColor: 'orange',
                                     width: '50px',
@@ -74,9 +74,8 @@ console.log(landingHeading)
                                     cursor: 'pointer'
                                 }} className="px-3 rounded-circle py-2"> <GoPlus color="white" /></div>
                             </div>
-                            
-                            <p className="text-white mt-3">{landingDes}</p>
-                            <p className="text-white mt-3"></p>
+
+                            <p className="text-white">{landingDes}</p>
                         </div>
                     </div>
                 </div>
