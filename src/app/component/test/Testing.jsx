3e8447741d2b246ca0d86a27591d1b95606bf4de
kartik <gaurdeveloper1916@gmail.com>
2025-05-10 15:04:47 +0530

    2 section

--- src/app/component/test/Testing.jsx
+++ src/app/component/test/Testing.jsx
@@ -20,31 +20,31 @@ const Testing = () => {
             heading: "Brand Association",
             description:
                 "Our flagship offerings include Brand Association and Capital Procurement—where we collaborate with industrial leaders to fuel strategic brand growth and redefine market dominance.",
-            image: "/piller/brand_association.webp"
+            image: "/home/brand_association.webp"
         },
         {
             heading: "Brand Development",
             description:
                 "We specialize in crafting compelling brand identities that resonate with your target audience. From brand positioning to storytelling, our strategies help establish a strong and lasting presence in the market.",
-            image: "/piller/Brand_Development.webp"
+            image: "/home/Brand_Development.webp"
         },
         {
             heading: "Marketing",
             description:
                 "Our expert marketing solutions span digital, traditional, and innovative strategies to connect brands with their audience effectively. Whether it's campaign management or analytics, we deliver measurable results.",
-            image: "/piller/brand_marketing.jpeg"
+            image: "/home/brand_marketing.jpeg"
         },
         {
             heading: "Print & Electronic Media",
             description:
                 "Partnering with top-tier media outlets worldwide, we provide unmatched visibility for your brand through expertly curated content in both print and electronic formats.",
-            image: "/piller/PRINT_MEDIA_MARKETING.webp"
+            image: "/home/PRINT_MEDIA_MARKETING.webp"
         },
         {
             heading: "Brand Seed Funding",
             description:
                 "We support emerging brands by facilitating seed funding opportunities. Our network of investors and industry experts ensures the financial backing needed to turn visionary ideas into reality.",
-            image: "/piller/brand_seed_funding.webp"
+            image: "/home/brand_seed_funding.webp"
         },
         {
             heading: "Brand Private Equity Offering",
@@ -56,7 +56,7 @@ const Testing = () => {
             heading: "B2B & B2C Events",
             description:
                 "As part of outdoor-marketing campaign, we organize and manage variety of B2B & B2C events for brands direct interactions with their consumers via our events globally.",
-            image: "./piller/b2b_b2c_events.webp"
+            image: "./home/b2b_b2c_events.webp"
         },
         {
             heading: "From Startups to Multinationals",
@@ -81,7 +81,7 @@ const Testing = () => {
             <div>
                 <div className="px-lg-5 px-3 px-md-5 background_video">
                     <video autoPlay loop muted className="video-background">
-                        <source src="/demigod.mp4" type="video/mp4" />
+                        <source src="./home/demigod.mp4" type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
                     <div className="d-flex p-0  postion-relative d-flex  vh-100 align-items-center justify-content-center ">
