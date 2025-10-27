4161d6699553a83aa2680ef3c77f05cb63db2f70
kartik <gaurdeveloper1916@gmail.com>
2025-10-28 04:47:37 +0530

    Fix Next.js 13+ build for Amplify static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true, 
-    images: {
-      domains: ["img.weddingbazaar.com","images.unsplash.com","media.istockphoto.com"],
-    },
-   
-  };
-  
-  module.exports = nextConfig;
-  
+  reactStrictMode: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+    domains: ["img.weddingbazaar.com", "images.unsplash.com", "media.istockphoto.com"],
+  },
+};
+
+module.exports = nextConfig;
