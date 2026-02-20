2def53d7ab5246b2bb1a18dd2a5963b3d6809692
kartik <gaurdeveloper1916@gmail.com>
2026-02-20 08:20:54 +0530

    add chnages

--- src/app/component/mnm/slider/Customslider.jsx
+++ src/app/component/mnm/slider/Customslider.jsx
@@ -1,6 +1,6 @@
 "use client";
-import React, {  } from "react";
-// import React, { useRef } from "react";
+// import React, {  } from "react";
+import React, { useRef } from "react";
 
 import Slider from "react-slick";
 import './index.css'
